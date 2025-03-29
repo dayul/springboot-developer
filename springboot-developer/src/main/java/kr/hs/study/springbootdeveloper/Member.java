62a718f4614102ce8ae6c5942911c89b19ecdb05
dayul <dyulchoo@naver.com>
2025-03-30 00:21:07 +0900

    Annotation 설명

--- springboot-developer/src/main/java/kr/hs/study/springbootdeveloper/Member.java
+++ springboot-developer/src/main/java/kr/hs/study/springbootdeveloper/Member.java
@@ -6,18 +6,18 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)      // 기본 생성자 생성
 @AllArgsConstructor
 @Getter
-@Entity
+@Entity(name = "member")     // 엔티티로 지정, 클래스와 테이블을 매핑 (이름 지정 가능)
 @Table(name = "member")
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id     // id 필드를 기본키로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     // 자동으로 기본키 1씩 증가(auto increment)
     @Column(name = "id", updatable = false)
     public Long id;        // DB 테이블의 'id' 컬럼과 매칭
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false)                // name이라는 not null 컬럼과 매핑
     private String name;    // DB 테이블의 'name' 컬럼과 매칭
 
 }
--- springboot-developer/src/main/java/kr/hs/study/springbootdeveloper/MemberRepository.java
+++ springboot-developer/src/main/java/kr/hs/study/springbootdeveloper/MemberRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {     // 상속 받을 때 엔티티와 엔티티의 기본키 타입을 넣어줌
 }
