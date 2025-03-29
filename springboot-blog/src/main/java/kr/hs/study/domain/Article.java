8a67e2cdc0797e2bee6d1b38c2401a3e25d23691
dayul <dyulchoo@naver.com>
2025-03-30 00:39:04 +0900

    Builder 패턴 사용(lombok 사용 후)

--- springboot-blog/src/main/java/kr/hs/study/domain/Article.java
+++ springboot-blog/src/main/java/kr/hs/study/domain/Article.java
@@ -1,9 +1,14 @@
 package kr.hs.study.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter     // lombok : 코드 반복 입력을 줄여서 가독성 향상
+@NoArgsConstructor(access = AccessLevel.PROTECTED)      // protected인 기본 생성자 생성
 public class Article {
 
     @Id
@@ -17,23 +22,9 @@ public class Article {
     @Column(name = "content", nullable = false)
     private String content;
 
-    @Builder        // builder 패턴 사용
+    @Builder        // builder 패턴으로 객체 생성
     public Article(String title, String content) {
         this.title = title;
         this.content = content;
-    }
-
-    protected Article() {}
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContent() {
-        return content;
     }
 }
